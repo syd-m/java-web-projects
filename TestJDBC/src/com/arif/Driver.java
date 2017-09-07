f486bf0ee5c781caadab2863158cdbaeefd6a7bc
Syed <mohsyd90@gmail.com>
2017-09-07 12:29:21 -0700

    commit

---
+++ TestJDBC/src/com/arif/Driver.java
@@ -0,0 +1,34 @@
+package com.arif;
+import java.sql.*;
+public class Driver {
+	//uploaded in git
+	public static void main(String[] args) {
+
+		String URL = "jdbc:mysql://localhost/hms";
+		String user = "root";
+		String pass = "password";
+		
+		try{
+			//1. Get Connection to Database 
+			Connection con = DriverManager.getConnection(URL, user, pass);
+			
+			//2. SQL Statement  
+			Statement statement = con.createStatement();
+			
+			//3. Executing SQL Query and getting into result set.
+			ResultSet res = statement.executeQuery("SELECT * FROM Booking");
+			
+			//4. Processing results 
+			while(res.next()){
+				System.out.println(res.getString("checkin") + " " + res.getString("checkout") + " " + res.getString("num_of_guests"));
+			}
+			
+		}
+		catch(Exception e){
+			e.getMessage();
+		}
+
+
+	}
+
+}
